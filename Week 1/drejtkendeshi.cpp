97aaed3563996ec0fbb355ed9d6c7b3552a7986a
Sara Simnica <Sarasimnica@Outlook.com>
2025-09-26 12:57:44 +0200

    Week 1

---
+++ Week 1/drejtkendeshi.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int brinjaA;
+
+    int brinjaB;
+
+    cout << "Vendos brinjen A: ";
+    cin >> brinjaA;
+    cout << "Vendos brinjen B: ";
+    cin >> brinjaB;
+
+    cout<< "Perimetri i drejtkendeshit eshte1: ";
+    cout<< 2*(brinjaA + brinjaB);
+    cout<< "Syprina e drejtkendeshit eshte: ";
+    cout<< brinjaA * brinjaB;
+
+    // double brinjaA;
+    // double brinjaB;
+    // string teksti = "Vendos brinjen: ";
+
+    // cout<< teksti << "a:";
+    // cin>> brinjaA;
+
+    // cout<<teksti<< "b:";
+    // cin>>brinjaB;
+
+    
+
+    return 0;
+ 
+}
---
+++ Week 1/hyrje.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // cout << "Programi Filloi";
+    // cout << endl;
+    // cout << "Pershendetje \n";
+    // cout << "Grupi 4b";
+
+    int numriIpare = 10;
+    cout<< "Vendose numrin e pare:";
+    cin >> numriIpare;
+    // cout << "Numri i pare eshte: \n" ;
+    // cout << numriIpare;
+
+    int numriIdyte = 11;
+    cout<< "Vendose numrin e dyte: ";
+    cin >> numriIdyte;
+    // cout << " Numri i dyte eshte: " ;
+    // cout << numriIdyte;
+
+
+    cout << "Shuma e numrave eshte:";
+    cout<< numriIpare + numriIdyte;
+
+
+
+
+ 
+    return 0;
+}
