8a1414d276fa915c7abc265d4213aedd05f7c429
Sara Simnica <Sarasimnica@Outlook.com>
2025-10-31 08:00:17 +0100

    update

---
+++ D.Sh/praktike/fibonacci.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter how many Fibonacci numbers you want: ";
+    cin >> n;
+
+    int a = 0, b = 1;
+
+    cout << "Fibonacci sequence: ";
+
+    for (int i = 0; i < n; i++) {
+        cout << a << " ";
+        int c = a + b;
+        a = b;
+        b = c;
+    }
+
+    cout << endl;
+    return 0;
+}
