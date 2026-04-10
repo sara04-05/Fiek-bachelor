b93e168a944ebf3ce0508c204f349e4cd6235a67
Sara Simnica <Sarasimnica@Outlook.com>
2026-04-10 09:02:27 +0200

    update

---
+++ sem2/ushtrime/week 7/stack.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+stack<int> krijostack(int a, int b, int c){
+ stack<int> stk;
+
+ while (stk.size() <a)
+ {
+    int x;
+    cout<< "Vendose nje numer:";
+    cin>>x;
+
+    if(x > b && x < c){
+        stk.push(x);
+    }
+}
+
+return stk;
+}
+
+int stackEdges(stack<int> stk, int &rez)
+{
+ rez = stk.top();
+
+ while(stk.size()> 1)
+ {
+    stk.pop();
+ }
+ return stk.top();
+}
+
+int main(){
+stack<int> st = krijostack(10, 12, 43);
+
+cout<< "I pari" << st.top() << endl;
+st.pop();
+cout<< "I dyti" << st.top() << endl;    
+
+int front;
+int back = stackEdges(st, front);
+cout<< "I pari" << front << endl;
+cout<< "I Fundit" << back << endl;
+
+    return 0;
+}
